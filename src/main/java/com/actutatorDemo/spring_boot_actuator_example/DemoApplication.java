4b0ab8f4e79ff749be406a9dde5f9979b0570190
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-08-29 03:47:09 -0400

    Completed

--- src/main/java/com/actutatorDemo/spring_boot_actuator_example/DemoApplication.java
+++ src/main/java/com/actutatorDemo/spring_boot_actuator_example/DemoApplication.java
@@ -1,4 +1,13 @@
 package com.actutatorDemo.spring_boot_actuator_example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
 }
